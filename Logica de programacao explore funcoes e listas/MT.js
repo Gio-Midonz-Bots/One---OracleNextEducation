59710b117254e4212ed5c49400162335f76e5be0
Gio-Midonz-Bots <roboticagiovanna@gmail.com>
2025-01-25 23:51:52 -0300

    Function

---
+++ Logica de programacao explore funcoes e listas/MT.js
@@ -0,0 +1,11 @@
+// manipulação de texto
+
+ let titulo = document.querySelector('h1');
+ titulo.innerHTML = 'Jogo do número secreto';
+ let paragrafo = document.querySelector('p');
+ paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
+
+
+function verificarChute(){
+    
+}
