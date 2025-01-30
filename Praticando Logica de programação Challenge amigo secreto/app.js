f86fd74799482aeab03dc19e0ca869e12938f706
Gio-Midonz-Bots <roboticagiovanna@gmail.com>
2025-01-30 09:57:26 -0300

    adicionar um amigo

--- Praticando Logica de programação Challenge amigo secreto/app.js
+++ Praticando Logica de programação Challenge amigo secreto/app.js
@@ -1,3 +1,24 @@
-let amigos = [];
+let nomeDeAmigos = [];
+// Explicação 
+// O campo de input cria uma caixa para que os dados sejam inseridos.
+// O querySelector recupera os dados inseridos.
+// O value acessa os dados inseridos e os disponibiliza para uso.
+function adicionarAmigo(){
+    let amigo = document.querySelector('.input-name').value;
 
+    if(amigo == ''){
+        alert('Por favor, insira um nome.');
+    }else{
+        nomeDeAmigos.push()
+        limparcaixa();
+    }
+}
 
+function exibirTextoNaTela(){
+    
+}
+
+function limparcaixa(){
+    amigo = document.querySelector('.input-name');
+    amigo.value = '';
+}
